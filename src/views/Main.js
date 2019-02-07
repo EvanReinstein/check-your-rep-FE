7dfdb0f6c6c289a18cb70b47b4d4c16ae845685d
EvanReinstein <edreins@gmail.com>
2019-02-06 16:22:26 -0800

    Adds functionality to send zipcode to the backend and get data back

--- src/views/Main.js
+++ src/views/Main.js
@@ -1,18 +1,40 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class Main extends Component {
+    state = {
+        zipcode: '',
+    }
+
     handleChange = (e) => {
         console.log(e.target.value);
         this.setState({
             [e.target.name]: e.target.value
         });
     }
+
+    handleSubmit = (e) => {
+        e.preventDefault();
+        const zipcode = this.state.zipcode;
+
+        axios({
+            method: 'post',
+            url: '/api/rep-info',
+            baseURL: 'http://localhost:4000',
+            data: {
+                zipcode: zipcode
+            }
+        })
+        .then(res => {console.log(res.data.officials)})
+        .catch(err => {console.log(err)});
+    }
+
     render() {
         return (
             <div>
                 <h1>Main App Component</h1>
                 <p>Form test: </p>
-                <form className="search-form" action="post">
+                <form className="search-form" action="post" onSubmit={this.handleSubmit}>
                     <label htmlFor="">Zipcode: </label>
                     <input
                     onInput={this.handleChange}
